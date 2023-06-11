69fd2101d0fb0f1feeee5b46992cb5abce22c986
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2023-06-11 20:36:30 +0300

    a

--- api.js
+++ api.js
@@ -1,23 +1,23 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const config = require("config");
-// const chalk = require("chalk");
-// const cors = require("cors");
-// const routes = require("./routes");
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const chalk = require("chalk");
+const cors = require("cors");
+const routes = require("./routes");
 
-// const initDatabase = require("./startUp/initDatabase");
-// const serverless = require("serverless-http");
-// const app = express();
+const initDatabase = require("./startUp/initDatabase");
+const serverless = require("serverless-http");
+const app = express();
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cors());
-// app.use("/upload", express.static("uploads"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use("/upload", express.static("uploads"));
 
-// app.use("/api", routes);
+app.use("/api", routes);
 
-// // const PORT = process.env.PORT || 8080;
-// const PORT = 4000;
+// const PORT = process.env.PORT || 8080;
+const PORT = 4000;
 
 // async function start() {
 //   try {
@@ -34,23 +34,30 @@
 //   }
 // }
 // start();
-
-const express = require("express");
-
-const app = express();
-const PORT = 4000;
-
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
+  console.log(chalk.green(`Server started on port:${PORT}`));
 });
 
 app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
+  res.send("HEELOW");
 });
 
-app.get("/about", (req, res) => {
-  res.send("This is my about route..... ");
-});
+// const express = require("express");
+
+// const app = express();
+// const PORT = 4000;
+
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `);
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hey this is my API running 🥳");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("This is my about route..... ");
+// });
 
 // app.use("/.netlify/functions/api", routes);
 module.exports = app;
