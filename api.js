7117baadf389c9f3db9c6146c079ad8d52467e05
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2023-06-11 20:40:01 +0300

    a

--- api.js
+++ api.js
@@ -9,10 +9,10 @@ const initDatabase = require("./startUp/initDatabase");
 const serverless = require("serverless-http");
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-app.use("/upload", express.static("uploads"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cors());
+// app.use("/upload", express.static("uploads"));
 
 // app.use("/api", routes);
 
