c1ec39ddc995bd1c9e270fb270baf852fa7dc666
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2023-06-11 20:19:02 +0300

    a

--- api.js
+++ api.js
@@ -16,7 +16,8 @@ app.use("/upload", express.static("uploads"));
 
 app.use("/api", routes);
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
+const PORT = 4000;
 
 async function start() {
   try {
