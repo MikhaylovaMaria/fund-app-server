679d4a2737b9e8f362dc00f2e9941670f3807591
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2023-06-11 20:33:35 +0300

    a

--- api.js
+++ api.js
@@ -1,59 +1,56 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const config = require("config");
-const chalk = require("chalk");
-const cors = require("cors");
-const routes = require("./routes");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const config = require("config");
+// const chalk = require("chalk");
+// const cors = require("cors");
+// const routes = require("./routes");
 
-const initDatabase = require("./startUp/initDatabase");
-const serverless = require("serverless-http");
-const app = express();
+// const initDatabase = require("./startUp/initDatabase");
+// const serverless = require("serverless-http");
+// const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-app.use("/upload", express.static("uploads"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cors());
+// app.use("/upload", express.static("uploads"));
 
-app.use("/api", routes);
+// app.use("/api", routes);
 
-// const PORT = process.env.PORT || 8080;
-const PORT = 4000;
+// // const PORT = process.env.PORT || 8080;
+// const PORT = 4000;
 
 // async function start() {
-try {
-  mongoose.connection.once("open", () => {
-    initDatabase();
-  });
-  await mongoose.connect(config.get("mongoUri"));
-  app.listen(PORT, () => {
-    console.log(chalk.green(`Server started on port:${PORT}`));
-  });
-} catch (error) {
-  console.log(chalk.red(e.message));
-  process.exit(1);
-}
-app.get("/", (req, res) => {
-  res.send("HELLO");
-});
+//   try {
+//     mongoose.connection.once("open", () => {
+//       initDatabase();
+//     });
+//     await mongoose.connect(config.get("mongoUri"));
+//     app.listen(PORT, () => {
+//       console.log(chalk.green(`Server started on port:${PORT}`));
+//     });
+//   } catch (error) {
+//     console.log(chalk.red(e.message));
+//     process.exit(1);
+//   }
 // }
 // start();
 
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
-// const PORT = 4000;
+const app = express();
+const PORT = 4000;
 
-// app.listen(PORT, () => {
-//   console.log(`API listening on PORT ${PORT} `);
-// });
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
 
-// app.get("/", (req, res) => {
-//   res.send("Hey this is my API running 🥳");
-// });
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
 
-// app.get("/about", (req, res) => {
-//   res.send("This is my about route..... ");
-// });
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
 
 // app.use("/.netlify/functions/api", routes);
 module.exports = app;
