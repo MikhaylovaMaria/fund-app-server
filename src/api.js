8159f14e686601dd2cab38fd1d5039ad54d104fe
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2023-06-11 21:11:07 +0300

    a

---
+++ src/api.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const chalk = require("chalk");
+const cors = require("cors");
+const routes = require("./routes");
+
+const initDatabase = require("./startUp/initDatabase");
+// const serverless = require("serverless-http");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use("/upload", express.static("uploads"));
+
+app.use("/api", routes);
+
+// const PORT = process.env.PORT || 8080;
+const PORT = 4000;
+
+async function start() {
+  try {
+    mongoose.connection.once("open", () => {
+      initDatabase();
+    });
+    await mongoose.connect(config.get("mongoUri"));
+    app.listen(PORT, () => {
+      console.log(chalk.green(`Server started on port:${PORT}`));
+    });
+  } catch (error) {
+    console.log(chalk.red(error.message));
+    process.exit(1);
+  }
+}
+start();
+
+module.exports = app;
